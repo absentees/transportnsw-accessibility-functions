49161b92e710813d5b0d2ad7adc6b69ebe572094
Scott <scott@pollen.com.au>
2019-12-02 09:04:05 +1100

    async callback mix

--- lib.js
+++ lib.js
@@ -1,34 +1,33 @@
 const stations = require("./nsw-train-stations.js");
-const tenStations = require('./ten.js');
+const tenStations = require("./ten.js");
 var Xray = require("x-ray");
 var x = Xray();
 
 var accessibilityRegex = new RegExp("(is not wheelchair accessible)", "g");
 
 async function getAllStations() {
-    try {
-        // TODO: Change this to all stations 
-        const promises = stations.map(async station => {
-            try {
-                const stationAccess = await getStationAccessibilityById(
-                    station.id
-                );
-                return {
-                    id: station.id,
-                    name: station.station,
-                    wheelchairAccess: stationAccess
-                };
-            } catch (error) {
-                console.log(error)
-            }           
-        });
+    // TODO: Change this to all stations
+    const promises = stations.map(async station => {
+        try {
+            const stationAccess = await getStationAccessibilityById(station.id);
+            return {
+                id: station.id,
+                name: station.station,
+                wheelchairAccess: stationAccess
+            };
+        } catch (error) {
+            console.log(`Error finding accessibility ${station.id}: ${error}`);
+            return {
+                id: station.id,
+                name: station.station,
+                wheelchairAccess: false
+            };
+        }
+    });
 
-        const result = await Promise.all(promises);
+    const result = await Promise.all(promises);
 
-        return result;
-    } catch (e) {
-        return Promise.reject(e.message);
-    }
+    return result;
 }
 
 async function getStationAccessibilityById(stationId) {
@@ -58,12 +57,16 @@ async function getStationId(stationName) {
     try {
         for (let station of stations) {
             if (station.alt.toLowerCase() === stationName.toLowerCase()) {
-                console.log(`Station name ${stationName} has id: ${station.id}`)
+                console.log(
+                    `Station name ${stationName} has id: ${station.id}`
+                );
                 return Promise.resolve(station.id);
             }
         }
 
-        return Promise.reject(`Station name: ${stationName} could not be found`);
+        return Promise.reject(
+            `Station name: ${stationName} could not be found`
+        );
     } catch (error) {
         return Promise.reject(error.message);
     }
